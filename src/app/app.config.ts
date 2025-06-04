3cd3439c087f89cb7868452ca1b68a8d9b7ec8d1
Vicente <vicente.ict3@gmail.com>
2025-06-04 19:43:10 +0200

    Added angular-calendar library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { EventsCalendar } from './components/events-calendar/events-calendar';
@@ -11,8 +12,9 @@ import { ToDoList } from './components/to-do-list/to-do-list';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
--- src/app/components/events-calendar/events-calendar.ts
+++ src/app/components/events-calendar/events-calendar.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalendarView, CalendarEvent, CalendarModule } from 'angular-calendar';
+import { startOfDay } from 'date-fns';
+
+import { CustomCalendarModule } from '../../shared/modules/calendar.module';
 
 @Component({
   selector: 'app-events-calendar',
-  imports: [],
+  imports: [CommonModule, CalendarModule, CustomCalendarModule],
   templateUrl: './events-calendar.html',
-  styleUrl: './events-calendar.css'
+  styleUrl: './events-calendar.css',
 })
 export class EventsCalendar {
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
 
+  events: CalendarEvent[] = [
+    {
+      start: startOfDay(new Date()),
+      title: 'Evento de ejemplo',
+    },
+  ];
+
+  handleDayClick(date: Date) {
+    const title = prompt('Título del evento:');
+    if (title) {
+      this.events = [
+        ...this.events,
+        {
+          start: startOfDay(date),
+          title,
+        },
+      ];
+    }
+  }
+
+  handleEventClick(event: CalendarEvent) {
+    const action = confirm(`¿Eliminar "${event.title}"?`) ? 'delete' : 'edit';
+
+    if (action === 'delete') {
+      this.events = this.events.filter((e) => e !== event);
+    } else {
+      const newTitle = prompt('Nuevo título:', event.title);
+      if (newTitle) {
+        event.title = newTitle;
+        this.events = [...this.events]; // fuerza actualización
+      }
+    }
+  }
 }
