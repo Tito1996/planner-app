51f355a6d1d44d49a3c7eb98a9b8786ba3a7d947
Vicente <vicente.ict3@gmail.com>
2026-03-21 17:31:30 +0100

    feat(home): principal components created

---
+++ src/app/features/home/components/graphics-panel/graphics-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics-panel',
+  imports: [],
+  templateUrl: './graphics-panel.html',
+  styleUrl: './graphics-panel.scss',
+})
+export class GraphicsPanel {}
---
+++ src/app/features/home/components/rutine-week-control/rutine-week-control.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rutine-week-control',
+  imports: [],
+  templateUrl: './rutine-week-control.html',
+  styleUrl: './rutine-week-control.scss',
+})
+export class RutineWeekControl {}
