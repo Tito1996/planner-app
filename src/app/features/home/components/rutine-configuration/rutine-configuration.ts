154aa0da3ca1cd224e1a2a845cdc55518986c032
Vicente <vicente.ict3@gmail.com>
2026-03-21 17:31:57 +0100

    feat(home): new component created

---
+++ src/app/features/home/components/rutine-configuration/rutine-configuration.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rutine-configuration',
+  imports: [],
+  templateUrl: './rutine-configuration.html',
+  styleUrl: './rutine-configuration.scss',
+})
+export class RutineConfiguration {}
